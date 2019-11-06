d9da9454c7c6e6b1f6bea4647d9653c3667cb4a1
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-06 22:48:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     when 1 
       return array.join
     when 2 
-      return array.join(", ")
+      return array.join(" and ")
     else
       array[-1] = "and " + array[-1]
       array = array.join(", ")
